36c648b45e82a8e4f2f938dd4040a8c5b24b25c7
nelsonmora48 <nelsonmora48@gmail.com>
2020-10-26 00:19:31 -0400

    Timeout Implementation and Handler Errors

--- handleEvent.js
+++ handleEvent.js
@@ -1,23 +1,37 @@
 const fetch = require("node-fetch");
+const AbortController = require("abort-controller");
+const controller = new AbortController();
+const timeout = setTimeout(() => {
+  controller.abort();
+}, 30000);
 
-module.exports = async (msg) => {
-  const content = JSON.parse(msg.content);
+module.exports = async (msg, url) => {
+  let response;
   console.log(" [*] Message Received");
   const json_msg = JSON.parse(msg.content.toString());
   // Fetch Data FROM External API
   try {
     console.log("Fetching...");
-    // const response = await fetch(
-    //   'https://stoq.com/q/l?s=" + json_msg.stock_code + "&f=sd2t2ohlcv&h&e=csv'
-    // );
-    const response = await fetch("http://localhost:1337/images/aapl.us.csv");
+    const final_url = url.replace(
+      "[stock_code]",
+      json_msg.stock_code
+    );
+    const response_fetch = await fetch(final_url, {
+      signal: controller.signal,
+    });
     console.log("Done...");
-    const buffer = await response.buffer();
-    // Parse Body Split by Lines, Gets second line and get de 6th position corresponding Current Quote
-    const body = buffer.toString().split("\r\n")[1].split(",")[6];
-    const resposne = (`${json_msg.stock_code.toUpperCase()} quote is $${body} per share`);
-    return {user: json_msg.user, response: resposne};
+    const buffer = await response_fetch.buffer();
+    // Parse Body Split by Lines, Gets second line and get de 6th position corresponding Current Quote (HOLC)
+    const quote = buffer.toString().split("\r\n")[1].split(",")[6];
+    if (quote !== "$N/D") {
+      response = `${json_msg.stock_code.toUpperCase()} quote is $${quote} per share`;
+    } else {
+      response = `${json_msg.stock_code.toUpperCase()} not Found`;
+    }
+    return { user: json_msg.user, response: response };
   } catch (error) {
-    console.error(error);
+    return { user: json_msg.user, response: "Bot has trouble getting data" };
+  } finally {
+    clearTimeout(timeout);
   }
 };
