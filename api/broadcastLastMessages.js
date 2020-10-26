6081985b51c9d150424253d588fdf4fe20304bb8
nelsonmora48 <nelsonmora48@gmail.com>
2020-10-26 07:16:33 -0400

    Correction in Query Last Messages

--- api/broadcastLastMessages.js
+++ api/broadcastLastMessages.js
@@ -1,11 +1,12 @@
+const _ = require('lodash');
 module.exports = async function (websocketId, room) {
   // Query Last 50 Messages from Database
   const last50Messages = await Post.find({
     where: { room: room },
     limit: 50,
-    sort: 'createdAt',
+    sort: 'createdAt DESC',
   });
 
   // Broadcast Result with Last 50 Messages
-  sails.sockets.broadcast(websocketId, 'message', last50Messages);
+  sails.sockets.broadcast(websocketId, 'message', _.reverse(last50Messages));
 };
