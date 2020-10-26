940797f0b36b968f29b5dfe601b4b80ca679da0d
nelsonmora48 <nelsonmora48@gmail.com>
2020-10-26 07:13:24 -0400

    ChatRooms

---
+++ src/Rooms.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import ChatBoard from "./ChatBoard";
+import socketIOClient from "socket.io-client";
+import sailsIOClient from "sails.io.js";
+
+var io = sailsIOClient(socketIOClient);
+io.sails.url = "http://localhost:1337";
+
+export default function Rooms() {
+  const [rooms, setRooms] = useState([]);
+  const [actualRoom, setActualRoom] = useState("");
+
+  const handleNewRoom = (e) => {
+    io.socket.get("/roomCreate");
+    setActualRoom("");
+  };
+
+  const handleJoin = (d) => {
+    io.socket.get("/join/" + d.target.name);
+    setActualRoom(d.target.name);
+  };
+
+  useEffect(() => {
+    io.socket.get("/rooms");
+    io.socket.on("rooms", (data) => setRooms(data));
+  }, []);
+
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th colSpan={2}>Chat Rooms:</th>
+          </tr>
+          {rooms.map((data, k) => (
+            <tr key={k}>
+              <td>{data}</td>
+              <td>
+                <button name={data} onClick={handleJoin}>
+                  Join
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>
+              <button onClick={handleNewRoom}>New Room</button>
+            </td>
+            <td></td>
+          </tr>
+        </tfoot>
+      </table>
+      {actualRoom !== "" && (
+        <ChatBoard
+          io={io}
+          actualRoom={actualRoom}
+          setActualRoom={setActualRoom}
+        />
+      )}
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Rooms from './Rooms'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Rooms /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
